a0042dcb024b3a13d8a3c55736f3daa21496a1eb
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-03 18:34:14 +0300

    create user model

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,32 @@
 const express = require('express');
-const Post = require('./../models/PostModel');
+const User = require('./../models/userModel');
 
 const router = express.Router();
+
+const adminLayout = '../views/layouts/admin';
+
+router.get('/admin', async (req, res) => {
+  try {
+    const locals = {
+      title: 'NodeJs Blog',
+      description: 'Simple Blog created with Node.js, Express & MongoDB',
+    };
+
+    res.render('admin/index', { locals, layout: adminLayout });
+  } catch (err) {}
+});
+
+router.post('/admin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (req.body.username === 'admin' && req.body.password === 'password') {
+      res.send('You are logged in.');
+    } else {
+      res.send('Wrong username or password');
+    }
+
+    res.redirect('/admin');
+  } catch (err) {}
+});
 
 module.exports = router;
